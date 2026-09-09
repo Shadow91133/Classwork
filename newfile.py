7b530d5a48d7746151e4aa4dc84cdb8f27aa4763
Shadow91133 <152953114+Shadow91133@users.noreply.github.com>
2026-09-09 22:27:17 +0500

    Add files via upload
    All tasks are done inside this one file

---
+++ newfile.py
@@ -0,0 +1,61 @@
+# Task 1
+
+print("This is Task 1 ")
+
+Name = input("type your name here : ")
+print(" hello "+Name)
+
+
+#Task 2
+
+print("This is Task 2")
+
+HoursWorked= int(input("Type in the hours you have worked : "))
+
+HourlyRate= int(input("Type in the hourly rate : "))
+
+print(HoursWorked*HourlyRate)
+
+#Task 3
+
+print("This is Task 3 ")
+
+C = int(input("Type in your celsius temperature : "))
+
+Fahrenheit = (C * 9/5) + 32
+
+print("This is Your",{Fahrenheit},"Fahrenheit Temperature")
+
+
+#Task 4
+
+print("This is Task 4")
+
+Num1 = int(input("Type in your First Number : "))
+Num2 =int(input("Type in your Second Number : "))
+
+Finalnum = (Num1**Num2)
+
+print("This is Your Final Number : ",{Finalnum})
+
+#Task 5
+
+print("This is Task 5")
+
+print("""age = 20
+item = 5
+print("age : ",age)
+print("items : ", item)
+
+age = float(age)
+item = float(item)
+print("age : ",age)
+print("items: ", item)""")
+
+explanation = ("Basically the upper written code Converts integer nume-rals into float numerals ")
+
+print(explanation)
+
+
+
+
